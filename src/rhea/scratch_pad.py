8b652399cf25157523b309d8e4fd4e8cdd9f6eec
subhojitdas <subhojitdss@gmail.com>
2025-04-09 02:55:58 +0530

    makemore bigram model

--- src/rhea/scratch_pad.py
+++ src/rhea/scratch_pad.py
@@ -7,9 +7,16 @@ import numpy as np
 #
 # X = np.random.normal(0, 1, (100, 2))
 # print(np.get_include())
-
-from src.titan.low.decision_tree import best_split
-arr = np.random.rand(18, 6)
-
-best_split(arr)
-
+#
+# from src.titan.low.decision_tree import best_split
+# arr = np.random.rand(18, 6)
+#
+# best_split(arr)
+import os
+os.environ["PATH"] += os.pathsep + "/opt/homebrew/bin"
+from graphviz import Digraph
+dot = Digraph()
+dot.node('A')
+dot.node('B')
+dot.edge('A', 'B')
+dot.render('test-output.gv', view=True)
