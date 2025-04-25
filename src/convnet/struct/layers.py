cfd45914979e2b1500ae7cc722bffe33df4310fc
subhojitdas <subhojitdss@gmail.com>
2025-04-25 17:32:30 +0530

    batchnorm forward pass

--- src/convnet/struct/layers.py
+++ src/convnet/struct/layers.py
@@ -13,15 +13,19 @@ class Linear:
 
     def forward(self, x):
         self.x = x
-        self.out = np.dot(x, self.weights, out=x) + self.bias
+        self.out = np.dot(x, self.weights) + self.bias
         return self.out
 
     def backward(self, dout):
-        dweights = np.dot(self.x.T, dout)
+        self.dweights = np.dot(self.x.T, dout)
         dx = np.dot(dout, self.weights.T)
         if self.have_bias:
-            dbias = np.sum(dout, axis=0)
-        return dx, dweights, dbias
+            self.dbias = np.sum(dout, axis=0)
+        return dx
+
+    def update_param(self, lr):
+        self.weights += -lr * self.dweights
+        self.bias += -lr * self.dbias
 
     def parameters(self):
         return [self.weights] + [self.bias] if self.have_bias else [self.weights]
@@ -41,3 +45,61 @@ class ReLU:
     def parameters(self):
         return []
 
+    def update_param(self, lr):
+        pass
+
+
+class BatchNorm1d:
+
+    def __init__(self, fan_in, eps=1e-5, momentum=0.9):
+        self.epsilon = 1e-5
+        self.momentum = momentum
+        self.training = True
+        self.gamma = np.ones_like(fan_in)
+        self.beta = np.zeros_like(fan_in)
+        self.running_mean = np.zeros(fan_in)
+        self.running_var = np.ones(fan_in)
+
+    def forward(self, x, training=True):
+        n = x.shape[0]
+        if self.training:
+            bnmeani = np.mean(x, axis=0, keepdims=True)
+            bnstdi = np.var(x, axis=0, keepdims=True)
+        else:
+            bnmeani = self.running_mean
+            bnstdi = self.running_var
+        bndiff = (x - bnmeani)
+        bndiff2 = bndiff**2
+        bnvar = 1/(n - 1)*bndiff2.sum(0, keepdims=True)
+        bnvar_inv = (bnvar + self.epsilon)**0.5
+        xhat = bndiff * bnvar_inv
+        self.out = self.gamma * xhat + self.beta
+        if self.training:
+            self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * bnmeani
+            self.running_var = (1 - self.momentum) * self.running_var + self.momentum * bnstdi
+        return self.out
+
+    def backward(self, out_grad):
+        pass
+
+
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+
+class Model:
+    def __init__(self, layers):
+        self.layers = layers
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def backward(self, grad):
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+
+    def update_param(self, lr):
+        for layer in self.layers:
+            layer.update_param(lr)
---
+++ src/convnet/struct/loss.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class SoftmaxCrossEntropyLoss:
+
+    def forward(self, logits, target):
+        self.logits = logits
+        self.target = target
+        epsilon = 1e-12
+        num_examples = logits.shape[0]
+        shifted_scores = logits - np.max(logits, axis=1, keepdims=True)
+        exp_scores = np.exp(shifted_scores)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        correct_log_probs = -np.log(probs[np.arange(num_examples), target] + epsilon)
+        loss = np.mean(correct_log_probs)
+        return loss
+
+    def softmax_numpy(self, x, axis=1):
+        x_shifted = x - np.max(x, axis=axis, keepdims=True)
+        exp_x = np.exp(x_shifted)
+        return exp_x / np.sum(exp_x, axis=axis, keepdims=True)
+
+    def backward(self):
+        probs = self.softmax_numpy(self.logits)
+        probs[np.arange(self.logits.shape[0]), self.target] -= 1
+        dlogits = probs / self.logits.shape[0]
+        return dlogits
