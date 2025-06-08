0b3cee72020a4597ba096326ed2c206ad89b57fb
subhojitdas <subhojitdss@gmail.com>
2025-06-09 01:00:00 +0530

    gpt2 pre-trained model generation

---
+++ src/gpt/hf_gpt2.py
@@ -0,0 +1,22 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+
+
+model_name = "gpt2"
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+model.eval()
+
+prompt = 'I love deep learning because '
+
+inputs = tokenizer(prompt, return_tensors="pt")
+
+with torch.no_grad():
+    outputs = model.generate(
+        input_ids=inputs["input_ids"],
+        max_length=512,
+        temperature=0.2,
+    )
+
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print(generated_text)
